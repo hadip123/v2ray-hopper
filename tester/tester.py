84eb481ecb0d36d9816afd065c2d5f6d4625b5a6
Hadi <xhopeter@proton.me>
2025-08-07 22:34:37 +0330

    Fixed the waiting for v2ray issue in tester.py

--- tester/tester.py
+++ tester/tester.py
@@ -18,15 +18,18 @@ formatted_date = dt.strftime("%Y_%m_%d__%H_%M_%S")
 for config_file in config_files:
     print(f"TESTING {config_file}")
     process = subprocess.Popen(
-        ["v2ray", "run", "-c", f"./configs/{config_file}"])
+        ["v2ray", "run", "-c", f"./configs/{config_file}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-    time.sleep(1.5)
+    for line in process.stdout:
+        if ("started" in line):
+            break
 
     print("REQUESTING")
 
     try:
         response = requests.get(
             "https://icanhazip.com", proxies=dict(http=http_proxy, https=https_proxy), timeout=2)
+        print(response.text)
         print("-----------------WORKED-------" + config_file)
         f = open(f"./wcp/working_configs_paths_{formatted_date}.txt", "a")
         f.write(config_file + "\n")
