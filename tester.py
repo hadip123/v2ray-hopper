c63cdb9e674040b58f134f3491c997ad1090a7c1
Hadi <mhn.hadi@outlook.com>
2025-08-07 13:08:59 +0330

    Bugs fixed; test ran successfully

--- tester.py
+++ tester.py
@@ -1,30 +1,33 @@
 import subprocess
 import os
 from time import sleep
+import random
 
 import requests
 
 config_files = os.listdir("configs/")
+random.shuffle(config_files)
 
 http_proxy = "socks5://127.0.0.1:10808"
 https_proxy = "socks5://127.0.0.1:10808"
 
 working_configs = []
-f = open("./working_configs_paths.txt", "w")
 for config_file in config_files:
     print(f"TESTING {config_file}")
     process = subprocess.Popen(
         ["v2ray", "run", "-c", f"./configs/{config_file}"])
 
-    sleep(1)
+    sleep(1.5)
 
     print("REQUESTING")
 
     try:
         response = requests.get(
             "https://icanhazip.com", proxies=dict(http=http_proxy, https=https_proxy), timeout=2)
-        print(response.body)
-        f.write(config_file)
+        print("-----------------WORKED-------" + config_file)
+        f = open("./working_configs_paths.txt", "a")
+        f.write(config_file + "\n")
+        f.close()
 
         working_configs.append(config_file)
     except Exception as e:
@@ -32,5 +35,3 @@ for config_file in config_files:
         continue
     finally:
         process.kill()
-
-f.close()
